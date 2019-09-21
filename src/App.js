1000c4e3243351b4f4c9d27c538bbbf114c773f1
vermathe1 <verma.the1@gmail.com>
2019-09-21 22:42:56 +0530

    fix indetaion in App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SearchFieldWIthLabel from './components/SearchFieldWIthLabel'
-import CreateTodo from './components/CreateTodo'
-import TodoList from './components/TodoList'
+import SearchFieldWIthLabel from './components/SearchFieldWIthLabel';
+import CreateTodo from './components/CreateTodo';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <header className="App-header">
       <SearchFieldWIthLabel
-        name = "Search"
-        onChange = { () =>{} }
-        label = "Todo Application"
-        placeholder = "Search"
-        type = "text"
+        name="Search"
+        onChange={() => {}}
+        label="Todo Application"
+        placeholder="Search"
+        type="text"
       />
       <CreateTodo
-    name = "add Todo"
-    onChange = {  () =>{}  }
-    placeholder = "Create Todo"
-    type = "text"
-    />
-    <TodoList
-    title = "I am title"
-    onDeleteCLick = { () =>{} }
-    todos = {[{task:"task1"}]}
-    />
+        name="add Todo"
+        onChange={() => {}}
+        placeholder="Create Todo"
+        type="text"
+      />
+      <TodoList
+        title="I am title"
+        onDeleteCLick={() => {}}
+        todos={[{ task: 'task1' }]}
+      />
     </header>
-   
   );
 }
 
